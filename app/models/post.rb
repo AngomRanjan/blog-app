ea6aaeaffe14af7c5bca689e328a487344e83630
AngomRanjan <angomranjan@gmail.com>
2022-10-27 10:52:50 +0530

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   def update_posts_counter
-    author.increment!(:post_counter)
+    author.increment!(:posts_counter)
   end
 
   def recent_comments
