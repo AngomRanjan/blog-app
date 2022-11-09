320871522e62a9e33ce200a90e5f9fc6b196fc6d
AngomRanjan <angomranjan@gmail.com>
2022-11-10 04:46:59 +0530

    Update Application Controller, Migrate DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
@@ -8,5 +12,11 @@ class User < ApplicationRecord
 
   def recent_posts
     posts.last(3)
+  end
+
+  private
+
+  def set_defaults
+    self.posts_counter ||= 0
   end
 end
