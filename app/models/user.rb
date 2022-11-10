861d57c0ad71bc595ac27195994795250c9620c7
AngomRanjan <angomranjan@gmail.com>
2022-11-10 07:28:05 +0530

    Enable User Registration And Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_initialize :set_defaults
+
   def recent_posts
     posts.last(3)
   end
